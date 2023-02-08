00b705faaa67a171d4029e9dbcdc67c3dac237d2
cggcbb <449567533@qq.com>
2023-02-08 21:27:56 +0800

    feat: update pinia store

--- src/store/setting.ts
+++ src/store/setting.ts
@@ -1,27 +1,59 @@
+export interface IWaterMarkSetting {
+  waterMarkActive: boolean,
+  waterMarkValue: string,
+  waterMarkType: string,
+  waterMarkImageUrl: string,
+  waterMarkSize: number,
+  waterMarkImageSize: number,
+  waterMarkColor: string,
+  waterMarkRotate: number,
+  watchMarkImageRotate: number,
+  waterMarkTypeOptions: IWaterMarkTypeOptionsItem[]
+}
+
+export interface IWaterMarkTypeOptionsItem {
+  label: string,
+  value: string
+}
+
 const useSetting = defineStore('setting', () => {
   const collapsed = ref(false)
   const menuWidth = ref(210)
-  const fullscreen = ref(false)
 
-  function setCollapsed(isCollapsed: boolean) {
+  const waterMarkSetting = reactive<IWaterMarkSetting>({
+    waterMarkActive: false,
+    waterMarkValue: '一匹正在奔腾的黑马',
+    waterMarkType: 'text',
+    waterMarkImageUrl: '',
+    waterMarkSize: 12,
+    waterMarkImageSize: 40,
+    waterMarkColor: 'rgba(128, 128, 128, .8)',
+    waterMarkRotate: -45,
+    watchMarkImageRotate: -30,
+    waterMarkTypeOptions: [{
+      label: "文字",
+      value: 'text',
+    },
+    {
+      label: '图片',
+      value: 'image'
+    }]
+  })
+
+  const setCollapsed = (isCollapsed: boolean): void => {
     collapsed.value = isCollapsed
   }
 
-  function setFullscreen(isScreen: boolean) {
-   fullscreen.value = isScreen
-  }
-
-  function setMenuWidth(width: number) {
+  const setMenuWidth = (width: number): void => {
     menuWidth.value = width
   }
 
   return {
     collapsed,
     menuWidth,
-    fullscreen,
     setCollapsed,
-    setFullscreen,
-    setMenuWidth
+    setMenuWidth,
+    waterMarkSetting
   }
 })
 
