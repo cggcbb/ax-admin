e42bcde486e98ffcc5816cd891039066d3d25809
cggcbb <449567533@qq.com>
2023-02-15 22:33:19 +0800

    refactor: update store song

--- src/store/song.ts
+++ src/store/song.ts
@@ -1,20 +1,14 @@
 const useSong = defineStore('song', () => {
+
   const playing = ref(false)
-  const volume = ref(100)
 
   const togglePlay = (play: boolean): void => {
     playing.value = play
   }
 
-  const setVolume = (v: number): void => {
-    volume.value = v
-  }
-
   return {
     playing,
-    volume,
     togglePlay,
-    setVolume
   }
 })
 
