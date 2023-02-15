7982055c0d3192c543fb95d76f905ae66ad32e90
cggcbb <449567533@qq.com>
2023-02-15 16:55:42 +0800

    fix: add useMessage to root page

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -80,6 +80,14 @@ router.beforeEach(() => {
 router.afterEach(() => {
   loadingBar.finish()
 })
+
+/*
+  如果你想在 setup 外使用信息
+  你需要在顶层 setup 中把 useMessage 返回的 message 值挂载到 window 下然后再调用
+  调用前需要确保 message 已经挂载成功
+*/
+window.$message = useMessage()
+
 </script>
 
 <style lang="less" scoped>
