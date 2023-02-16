451fd584ad7bf16c9db585a4ec3d0dd60e8cf0a6
cggcbb <449567533@qq.com>
2023-02-16 18:50:55 +0800

    feat: update header

--- src/components/ax-header/index.vue
+++ src/components/ax-header/index.vue
@@ -1,28 +1,50 @@
 <template>
   <div class="ax-header-layout">
-    <logo />
+    <logo v-if="showLogo" />
+    <div flex items-center v-if="showTitle">
+      <span fw-bold :class="[collapsed ? 'close-title' : 'show-title']">
+        ax-admin
+      </span>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
 export default defineComponent({
-  name: 'AXHeader',
+  name: 'AXHeader'
 })
+</script>
+
+<script lang="ts" setup>
+import useSetting from '~/store/setting'
+const setting = useSetting()
+
+const { collapsed } = toRefs(setting.menuSetting)
+const { showLogo, showTitle } = toRefs(setting.headerSetting)
 </script>
 
 <style scoped lang="less">
 .ax-header-layout {
-  height: @logoHeight;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 999;
   display: flex;
+  justify-content: center;
   align-items: center;
+  width: 100%;
+  height: @logoHeight;
+  z-index: 999;
   box-sizing: border-box;
-  border-bottom: 1px solid var(--border-color);
+  border-bottom: 1px dashed var(--border-color);
+
+  .show-title {
+    transform: scale(1);
+    width: auto;
+    transition: transform 0.2s ease-in;
+    margin-left: 10px;
+  }
+
+  .close-title {
+    transform: scale(0);
+    width: 0;
+    margin-left: 0;
+  }
 }
 </style>
--- src/components/logo/index.vue
+++ src/components/logo/index.vue
@@ -1,51 +1,18 @@
 <template>
   <div flex-center-all class="logo-wrapper">
-    <img v-if="showLogo" w-30px src="../../assets/logo.svg" />
-    <div flex items-center>
-      <span fw-bold :class="[menuSetting.collapsed ? 'close-title' : 'show-title']">ax-admin</span>
-    </div>
+    <img w-30px src="../../assets/logo.svg" />
   </div>
 </template>
 
 <script lang="ts">
 export default defineComponent({
-  name: 'Logo',
+  name: 'Logo'
 })
 </script>
 
-<script lang="ts" setup>
-import useSetting from '~/store/setting'
-
-defineProps({
-  showTitle: {
-    type: Boolean,
-    default: true,
-  },
-  showLogo: {
-    type: Boolean,
-    default: true,
-  },
-})
-
-const menuSetting = useSetting().menuSetting
-
-</script>
+<script lang="ts" setup></script>
 <style lang="less" scoped>
 .logo-wrapper {
   height: @logoHeight;
-  border-bottom: 1px dashed var(--border-color);
-
-  .show-title {
-    transform: scale(1);
-    width: auto;
-    transition: transform 0.2s ease-in;
-    margin-left: 10px;
-  }
-
-  .close-title {
-    transform: scale(0);
-    width: 0;
-    margin-left: 0;
-  }
 }
 </style>
