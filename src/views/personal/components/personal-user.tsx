e8061d90927f010fb96918a6526b0d544fc3dac2
cggcbb <449567533@qq.com>
2023-03-01 11:41:06 +0800

    refactor: update data to props

--- src/views/personal/components/personal-user.tsx
+++ src/views/personal/components/personal-user.tsx
@@ -1,41 +1,34 @@
 import { NCard, NAvatar, NSpace, NEl, NTag } from 'naive-ui'
-import { GET_PERSONAL } from '~/api/url'
 import { IPersonal } from '~/types/common'
-import { get } from '~/utils/http'
+import type { PropType } from 'vue'
 
 export default defineComponent({
   name: 'PersonalUser',
-
-  setup() {
-    let personalUser = $ref<IPersonal>()
-
-    onMounted(async () => {
-      const res = await get<IPersonal>(GET_PERSONAL)
-      personalUser = res.data
-    })
-
+  props: {
+    data: Object as PropType<IPersonal>
+  },
+  setup(props) {
     return () => (
       <>
-        <NCard style="min-height: 400px">
+        <NCard style="min-height: 400px" bordered={false}>
           <NSpace justify="center">
-            <NAvatar bordered round size={120} src={personalUser?.avatarUrl} />
+            <NAvatar bordered round size={120} src={props.data?.avatarUrl} />
           </NSpace>
           <NSpace justify="center" class="mt-1">
-            <span class="text-6">{personalUser?.name}</span>
+            <span class="text-6">{props.data?.name}</span>
           </NSpace>
           <NSpace justify="center">
-            <span>{personalUser?.motto}</span>
+            <span>{props.data?.motto}</span>
           </NSpace>
           <NEl class="mt-3 w-100% ma-auto">
-            {personalUser?.content.map((item, index) => (
+            {props.data?.content.map((item, index) => (
               <NEl class={['center-layout', index !== 0 ? 'mt-2' : '']}>
-                <span class="w-45% inline-block text-right">{item.key}：</span>
-                <span class="w-55%">{item.value}</span>
+                {item.key}：{item.value}
               </NEl>
             ))}
           </NEl>
           <NSpace justify="center" class="mt-5">
-            {personalUser?.tags.map(item => (
+            {props.data?.tags.map(item => (
               <NTag type={item.type}>{item.text}</NTag>
             ))}
           </NSpace>
